5639c66807f6f9427ea63cb697d66a465ca485aa
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-26 17:23:40 -0500

    removed extra blank lines of code

--- pe002.rb
+++ pe002.rb
@@ -1,9 +1,6 @@
 fibonacci_numbers = [1,2]
-
 while fibonacci_numbers.last < 4000000
   fibonacci_numbers << fibonacci_numbers[-1] + fibonacci_numbers[-2]
 end
-
 fibonacci_numbers.pop if fibonacci_numbers.last >= 4000000
-
 puts sum = fibonacci_numbers.select { |x| x.even? }.reduce(:+)
