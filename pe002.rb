646d090eaef074b18a2b6f54cbce7c25822ec8de
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-26 17:20:59 -0500

    removed recursive method and solved fib numbers linearly

--- pe002.rb
+++ pe002.rb
@@ -1,17 +1,9 @@
-def fibonacci(sequence_number)
-  if sequence_number == 1
-    0
-  elsif sequence_number == 2
-    1
-  else
-    fibonacci(sequence_number - 1) + fibonacci(sequence_number - 2)
-  end
+fibonacci_numbers = [1,2]
+
+while fibonacci_numbers.last < 4000000
+  fibonacci_numbers << fibonacci_numbers[-1] + fibonacci_numbers[-2]
 end
 
-while (fib_nums ||= []).last == nil || fib_nums.last < 4000000
-  fib_nums << fibonacci(x ||= 1)
-  x += 1
-end
-fib_nums.pop if fib_nums.last >= 4000000
+fibonacci_numbers.pop if fibonacci_numbers.last >= 4000000
 
-puts sum = fib_nums.select { |x| x.even? }.inject(:+)
+puts sum = fibonacci_numbers.select { |x| x.even? }.reduce(:+)
