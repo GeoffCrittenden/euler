48bbaa1d07be4bd8126ae6ad33853b19829d06fa
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-27 16:22:48 -0500

    refactored algorithm using algebra instead of brute force

--- pe009.rb
+++ pe009.rb
@@ -1,11 +1,24 @@
-(1..997).each { |a|
-  (2..997).each { |b|
-    (3..997).each { |c|
-      next unless a < b && b < c
-      if a**2 + b**2 == c**2 && a + b + c == 1000
-        puts "#{[a,b,c]}, #{a * b * c}"
-        exit
-      end
-    }
-  }
-}
+t = Time.now
+# (1..997).each { |a|
+#   (2..997).each { |b|
+#     (3..997).each { |c|
+#       next unless a < b && b < c
+#       if a + b + c == 1000 && a**2 + b**2 == c**2
+#         puts "#{[a,b,c]}, #{a * b * c}"
+#         puts "#{Time.now - t} seconds"
+#         exit
+#       end
+#     }
+#   }
+# }
+
+n = 1000
+for a in 1..997
+  b = (n * (n - (2 * a))) / (2 * (n - a))
+  c = n - a - b
+  if a**2 + b**2 == c**2
+    puts "#{[a,b,c]}, #{a * b * c}"
+    puts "#{Time.now - t} seconds"
+    exit
+  end
+end
