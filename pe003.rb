694d4ce9427a01e9052afda56476a0a562064643
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-26 20:24:20 -0500

    refactoring and changing variable names

--- pe003.rb
+++ pe003.rb
@@ -1,35 +1,36 @@
 class MaxPrimeFactorFinder
+  attr_reader :max_prime_factor
 
   def initialize(number = 600851475143)
     @number = number
-    @factors = find_factors
-    @max_prime = find_max_prime
+    @max_prime_factor = find_max_prime_factor
   end
 
-  def find_factors(factors = [@number], divisor = 2)
-    number = @number
-    while divisor <= number
+  private
+
+  def find_max_prime_factor(number = @number, divisor = 2)
+    while divisor <= number / 2
       if number % divisor == 0
-        factors << number / divisor
         number /= divisor
       else
         divisor += 1
       end
     end
-    factors
+    number
   end
 
-  def find_max_prime
-    @factors.each { |factor| return factor if prime?(factor) }
-  end
+  # def find_max_prime
+  #   @factors.each { |factor| return factor if prime?(factor) }
+  # end
 
-  def prime?(number)
-    (2..number).each do |factor|
-      return false if number != factor && number % factor == 0
-    end
-    true
-  end
+  # def prime?(number)
+  #   (2..number).each do |factor|
+  #     return false if number != factor && number % factor == 0
+  #   end
+  #   true
+  # end
 
 end
 
 num = MaxPrimeFactorFinder.new
+p num.max_prime_factor
