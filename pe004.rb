35c118f98eef05c55903195a736141be21a097d7
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-26 22:12:44 -0500

    minor refactoring

--- pe004.rb
+++ pe004.rb
@@ -1,11 +1,12 @@
 max_pal = [0,0,0]
 (100..999).to_a.reverse.each do |num1|
   (100..999).to_a.reverse.each do |num2|
-    if num1 <= max_pal[1] && num1 <= max_pal[2] && num2 <= max_pal[1] && num2 <= max_pal[2]
+    if num2 <= [max_pal[1],max_pal[2]].min && num1 <= [max_pal[1],max_pal[2]].min
       exit
     elsif (num1 * num2).to_s == (num1 * num2).to_s.reverse && num1 * num2 > max_pal[0]
       max_pal[0] = num1 * num2; max_pal[1] = num1; max_pal[2] = num2
       p max_pal
+      break
     end
   end
 end
