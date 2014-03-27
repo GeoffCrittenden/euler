d1fb8417b223f5b432a440f11bc138c741217272
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-26 20:35:19 -0500

    removed class to simplify code

--- pe003.rb
+++ pe003.rb
@@ -1,25 +1,12 @@
-class MaxPrimeFactorFinder
-  attr_reader :max_prime_factor
-
-  def initialize(number = 600851475143)
-    @number = number
-    @max_prime_factor = find_max_prime_factor
-  end
-
-  private
-
-  def find_max_prime_factor(number = @number, divisor = 2)
-    while divisor <= number / 2
-      if number % divisor == 0
-        number /= divisor
-      else
-        divisor += 1
-      end
+def find_max_prime_factor(number = 600851475143)
+  while (divisor ||= 2) <= number / 2
+    if number % divisor == 0
+      number /= divisor
+    else
+      divisor += 1
     end
-    number
   end
-
+  number
 end
 
-num = MaxPrimeFactorFinder.new
-p num.max_prime_factor
+p find_max_prime_factor
