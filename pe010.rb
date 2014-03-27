f08d281c18a8025a6f0bf016a6f091fc670b6078
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-27 18:21:08 -0500

    created file for #10

---
+++ pe010.rb
@@ -0,0 +1,15 @@
+num = 2
+divisor = 2
+primes = []
+while primes.count < 10001
+  until divisor > num / 2
+    if num % divisor == 0
+      break     
+    else
+      divisor += 1
+    end
+  end
+  primes << num unless num > divisor && num % divisor == 0
+  num += 1
+  divisor = 2
+end
