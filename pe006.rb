055b19bb2ac07b89140b5780c497378ddcaed023
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-27 10:23:42 -0500

    removed unecessary parentheses

--- pe006.rb
+++ pe006.rb
@@ -1,7 +1,7 @@
-sum_squares = 0; square_sum = 0
+sum_squares = 0; sum_to_square = 0
 for x in 1..100
   sum_squares += x**2
-  square_sum += x
+  sum_to_square += x
 end
 
-p (square_sum)**2 - sum_squares
+p sum_to_square**2 - sum_squares
