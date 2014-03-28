4da41eba7e3531c6cd5cda0c363cd94b10469a08
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-28 13:05:16 -0500

    optimizing, got down to 10 seconds

--- pe010.rb
+++ pe010.rb
@@ -1,40 +1,12 @@
-# num = 2
-# divisor = 2
-# primes = [0]
-# count = primes.count
-# while primes.last < 2_000_000
-#   until divisor > num / 2
-#     if num % divisor == 0
-#       break     
-#     else
-#       divisor += 1
-#     end
-#   end
-#   primes << num unless num > divisor && num % divisor == 0
-#   p primes.last if count != primes.count
-#   count = primes.count
-#   if num.even?
-#     num += 1
-#   else
-#     num += 2
-#   end
-#   divisor = 2
-# end
-
-# primes.pop if primes.last >= 2_000_000
-# p primes.reduce(:+)
-
-# require 'Prime'
-
-# p Prime.each(2_000_000).map.reduce(:+)
-
 t = Time.now
-primes = []
-for number in 2..2000000 do
-  p number
-  primes << number if primes.none? { |prime| number % prime == 0 }
-  # p Time.now - t
+upper_bound = ARGV[0].to_i
+prime_numbers = (2..upper_bound).to_a
+multiples = []
+for n in 2..Math.sqrt(upper_bound)
+  for i in 0..prime_numbers.count
+    break if (n**2 + i*n) > prime_numbers.last
+    multiples << n**2 + i*n
+  end
 end
+p (prime_numbers - multiples).reduce(:+)
 p Time.now - t
-p primes
-p primes.reduce(:+)
