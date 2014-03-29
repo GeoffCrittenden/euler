19dfddebd94fbcccac6c368acf1a8bccf0158bbe
Geoff Crittenden <geoffcritt@hotmail.com>
2014-03-28 22:25:49 -0500

    experimenting with optimization

--- pe010.rb
+++ pe010.rb
@@ -3,7 +3,8 @@ upper_bound = 2000000
 prime_numbers = (3..upper_bound).map { |n| n if n.odd? }.compact
 prime_numbers.unshift(2)
 multiples = []
-for n in 3..Math.sqrt(upper_bound)
+sqrt = (upper_bound**0.5).to_i
+for n in 3..sqrt
   next if n.even?
   for i in 0..prime_numbers.count
     break if (n**2 + i*n) > prime_numbers.last
